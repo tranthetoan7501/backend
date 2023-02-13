33d0223d949c39e835f4afb128231edb664cd64e
tranthetoan7501 <68367691+tranthetoan7501@users.noreply.github.com>
2023-02-13 11:49:04 +0700

    s

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 const connectDB = async () => {
-  console.log('url database: ', process.env.DATABASE);
-  const conn = await mongoose.connect(process.env.DATABASE);
+  console.log(
+    'url database: ',
+    process.env.DATABASE ||
+      'mongodb+srv://tranthetoan:752001@cluster0.qcztah8.mongodb.net/bloger?retryWrites=true&w=majority'
+  );
+  const conn = await mongoose.connect(
+    process.env.DATABASE ||
+      'mongodb+srv://tranthetoan:752001@cluster0.qcztah8.mongodb.net/bloger?retryWrites=true&w=majority'
+  );
   console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
 
